857ea1b3f3bc54c50a59e062567cc241e5a4938d
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-19 18:41:17 +0300

    Added lib.rs

--- HelloWorld/target/debug/deps/HelloWorld-22b030e6bb70d1b9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sydyncc\hello_world\HelloWorld\target\debug\deps\HelloWorld-22b030e6bb70d1b9.rmeta: src\main.rs
-
-c:\Users\sydyncc\hello_world\HelloWorld\target\debug\deps\HelloWorld-22b030e6bb70d1b9.d: src\main.rs
-
-src\main.rs:
--- HelloWorld/target/debug/deps/HelloWorld.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sydyncc\hello_world\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
-
-C:\Users\sydyncc\hello_world\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
-
-src\main.rs:
