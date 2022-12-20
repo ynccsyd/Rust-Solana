3ff042aa33eec84bacfaa6f87af718f41106b2ca
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-20 13:18:09 +0300

    Added MovieClient

---
+++ client/README.md
@@ -0,0 +1,11 @@
+### Interact with your deployed program
+
+
+
+First you'll need to get your program ID. This can be found under the "Program Credentials" tab:
+
+![image](https://user-images.githubusercontent.com/109158340/208468566-357810be-81fd-4607-a7ec-f42680d41c8a.png)
+![image](https://user-images.githubusercontent.com/109158340/208468861-92b5d7e3-4611-47ea-b905-c6e1c95d6a7c.png)
+
+https://explorer.solana.com/tx/4cAn6kELoCW9wj6w8HZ77N1PHVQBHwUuQg7cnHq5PSFvbnrk2jREBC6Aqm9KKvSFSbopJvhdFspePEuAvKEHxpHD?cluster=devnet
+![image](https://user-images.githubusercontent.com/109158340/208469013-0f65eb31-9c35-4648-89ef-19239b342a6e.png)
---
+++ client/client.ts
@@ -0,0 +1,35 @@
+const programId = new web3.PublicKey(
+    "Write your program Id from solana playground"
+  );
+  
+  async function sayHello(
+    payer: web3.Keypair
+  ): Promise<web3.TransactionSignature> {
+    const transaction = new web3.Transaction();
+  
+    const instruction = new web3.TransactionInstruction({
+      keys: [], // We're not using any accounts yet
+      programId,
+      // No need to add data here!
+    });
+  
+    transaction.add(instruction);
+  
+    const transactionSignature = await web3.sendAndConfirmTransaction(
+      pg.connection,
+      transaction,
+      [payer]
+    );
+  
+    return transactionSignature;
+  }
+  
+  async function main() {
+    const transactionSignature = await sayHello(pg.wallet.keypair);
+  
+    console.log(
+      `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    );
+  }
+  
+  main();
